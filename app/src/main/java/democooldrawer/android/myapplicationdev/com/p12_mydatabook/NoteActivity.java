63b02b72e9e4411fbb38d7d45e0e826f66170e9e
127589 <127589@myrp.edu.sg>
2017-08-08 15:30:38 +0800

    Note activity

---
+++ app/src/main/java/democooldrawer/android/myapplicationdev/com/p12_mydatabook/NoteActivity.java
@@ -0,0 +1,13 @@
+package democooldrawer.android.myapplicationdev.com.p12_mydatabook;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NoteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note);
+    }
+}
