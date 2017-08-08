6c4d7bd9371e0b528c8e7b139c2e832a07c1e7fe
15017135 <15017135@myrp.edu.sg>
2017-08-08 15:47:10 +0800

    enhancement

--- app/src/main/java/democooldrawer/android/myapplicationdev/com/p12_mydatabook/AboutFragment.java
+++ app/src/main/java/democooldrawer/android/myapplicationdev/com/p12_mydatabook/AboutFragment.java
@@ -9,6 +9,8 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.squareup.picasso.Picasso;
+
 
 /**
  * A simple {@link Fragment} subclass.
@@ -32,6 +34,9 @@ public class AboutFragment extends Fragment {
         View view = inflater.inflate(R.layout.aboutfragment, container, false);
         tvAbout = (TextView)view.findViewById(R.id.tvAbout);
         ivAbout = (ImageView)view.findViewById(R.id.iv);
+        String url = "https://upload.wikimedia.org/wikipedia/commons/8/80/Republic_Polytechnic_Logo.jpg";
+        Picasso.with(view.getContext()).load(url).into(ivAbout);
+        return view;
     }
 
 }
